e0ab6e9a36dc8020a5d2ff9e42d6ff4e63173407
André Leão <andrefelipesl123@gmail.com>
2025-04-18 22:33:49 -0300

    Product Details

--- src/modules/ProductDetailsPage/components/CapacitySelector/CapacitySelector.tsx
+++ src/modules/ProductDetailsPage/components/CapacitySelector/CapacitySelector.tsx
@@ -1,20 +1,46 @@
+import { useContext, useState } from 'react';
 import styles from './CapatitySelector.module.scss';
 import cn from 'classnames';
+import { ProductDetailsContext } from 'store/ProductDetailsContext';
 
 export const CapacitySelector = () => {
-  const capacities = ['64 GB', '256 GB', '512 GB'];
-  const isActive = true;
+  const { product } = useContext(ProductDetailsContext);
+
+  const formatText = (text: string) => {
+    const prefix = text.slice(0, -2);
+    const suffix = text.slice(-2);
+
+    return `${prefix} ${suffix}`;
+  };
+
+  const [selectedCapacity, setSelectedCapacity] = useState(
+    product ? formatText(product.capacity) : '',
+  );
+
+  if (!product) {
+    return;
+  }
+
+  const formattedList = product.capacityAvailable.map(capacity => {
+    return formatText(capacity);
+  });
+
+  const handleOnCapacityChange = (capacity: string) => {
+    setSelectedCapacity(capacity);
+  };
 
   return (
     <div className={styles.container}>
       <span className={styles.container__label}>Select capacity</span>
       <div className={styles.container__items}>
-        {capacities.map(capacity => (
+        {formattedList.map(capacity => (
           <div
             key={capacity}
             className={cn(styles.container__items__item, {
-              [styles.container__items__item__active]: isActive,
+              [styles.container__items__item__active]:
+                selectedCapacity === capacity,
             })}
+            onClick={() => handleOnCapacityChange(capacity)}
           >
             <span>{capacity}</span>
           </div>
